31432bc5ada2fca623aa7909d5e69f648482e918
Lorena Sarasua <lorena.sarasua@gmail.com>
2017-05-22 14:38:00 -0400

    changed the names of the 3 filters. Fixed general spacing for legibility. Added funny microsoft staple on empty pages.

--- src/components/ToDoApp.js
+++ src/components/ToDoApp.js
@@ -4,42 +4,50 @@ import VisibleToDoList from './VisibleToDoList';
 class ToDoApp extends React.Component {
 	constructor() {
 		super();
-		this.visibilityFilters = ["ALL_TODOS", "LEFT_TODOS", "COMPLETED_TODOS"];
+		this.visibilityFilters = ["all", "to do", "done"];
 		this.state = {
 			toDoList: this.props ? this.props.dataInterface.getAllToDoItems() : [],
-			visibilityFilter: "ALL_TODOS"
+			visibilityFilter: "all"
 		};
 	}
 
 	addToDoItem = () => {
 		if (this._toDoInputField.value) {
 			this.props.dataInterface.addToDoItem(this._toDoInputField.value);
-			this.setState({toDoList: this.props.dataInterface.getAllToDoItems()});
+			this.setState({
+				toDoList: this.props.dataInterface.getAllToDoItems()
+			});
 			this._toDoInputField.value = '';
 		}
 	}
 
 	archiveToggleToDoItem = e => {
 		this.props.dataInterface.archiveToggleToDoItem(e.target.dataset.id);
-		this.setState({toDoList: this.props.dataInterface.getAllToDoItems()});
+		this.setState({
+			toDoList: this.props.dataInterface.getAllToDoItems()
+		});
 	}
 
 	removeToDoItem = e => {
 		this.props.dataInterface.removeToDoItem(e.target.dataset.id);
-		this.setState({toDoList: this.props.dataInterface.getAllToDoItems()});
+		this.setState({
+			toDoList: this.props.dataInterface.getAllToDoItems()
+		});
 	}
 
 	changeVisibilityFilter = e => {
-		this.setState({visibilityFilter: e.target.dataset.id});
+		this.setState({
+			visibilityFilter: e.target.dataset.id
+		});
 	}
 
 	visibleToDos = () => {
 		switch (this.state.visibilityFilter) {
-			case "ALL_TODOS": 
+			case "all": 
 				return this.state.toDoList;
-			case "LEFT_TODOS":
+			case "to do":
 				return this.state.toDoList.filter(item => item.isDone === false);
-			case "COMPLETED_TODOS":
+			case "done":
 				return this.state.toDoList.filter(item => item.isDone === true);
 			default: 
 				return this.state.toDoList;			
@@ -53,48 +61,53 @@ class ToDoApp extends React.Component {
 	render() {
 		let visibleToDos = this.visibleToDos();
 		return (
-			<div className="app-container">
-				<h2>do.</h2>
-				<form 
-					onSubmit = {
-						(e) => {
-							e.preventDefault();
-						}
-					}>
-					<input
-						type = "text"
-						placeholder = "add a task..."
-						ref = {
-							(input => this._toDoInputField = input)
-						}
-					/>
-					<button 
-						type = "submit"
-						onClick = {this.addToDoItem}>
-							+
-					</button>
-				</form>	
-				<div>
-					{
-						this.visibilityFilters.map (
-							visibilityFilter => 
-								<button
-									type = "button"
-									key = {visibilityFilter}
-									onClick = {this.changeVisibilityFilter}
-									data-id = {visibilityFilter}>
-										{visibilityFilter.replace("_", " ")}
-								</button>
-						)
-					}
+				<div className="app-container">
+					<div className="top-section">
+						<div className="header">
+							<h2 className="title">do.</h2>
+							<form 
+								onSubmit = {
+									(e) => {
+										e.preventDefault();
+									}
+								}>
+								<input
+									type = "text"
+									placeholder = "add a task..."
+									ref = {
+										(input => this._toDoInputField = input)
+									}
+								/>
+								<button 
+									className = "add-button"
+									type = "submit"
+									onClick = {this.addToDoItem}>
+										+
+								</button>		
+							</form>
+						</div>			
+						<div className="filter-nav">
+							{
+								this.visibilityFilters.map (
+									visibilityFilter => 
+										<button
+											type = "button"
+											key = {visibilityFilter}
+											onClick = {this.changeVisibilityFilter}
+											data-id = {visibilityFilter}>
+												{visibilityFilter}
+										</button>
+								)
+							}
+						</div>
+					</div>	
+					<VisibleToDoList
+						visibleToDos = {visibleToDos}
+						visibilityFilter = {this.state.visibilityFilter}
+						archiveToggleToDoItem = {this.archiveToggleToDoItem}
+						removeToDoItem = {this.removeToDoItem}
+					/>					
 				</div>
-				<VisibleToDoList
-					visibleToDos = {visibleToDos}
-					visibilityFilter = {this.state.visibilityFilter}
-					archiveToggleToDoItem = {this.archiveToggleToDoItem}
-					removeToDoItem = {this.removeToDoItem}
-				/>					
-			</div>
 		);
 	}
 }
